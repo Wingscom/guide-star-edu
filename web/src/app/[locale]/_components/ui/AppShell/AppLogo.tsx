b8940bf0a8ecdad2ce34ac72e583cc106ea421e3
Hoai Thai <khanhhoai95@gmail.com>
2024-03-13 23:27:52 +0700

    feat: update logo

--- web/src/app/[locale]/_components/ui/AppShell/AppLogo.tsx
+++ web/src/app/[locale]/_components/ui/AppShell/AppLogo.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { useMantineColorScheme } from "@mantine/core";
 import Image from "next/image";
 
 export function AppLogo() {
-  const { colorScheme } = useMantineColorScheme();
+  // TODO: Use logo for dark theme
+  // const { colorScheme } = useMantineColorScheme();
 
-  if (colorScheme === "dark")
-    return <Image src="/logo-white.png" alt="logo" width={83} height={50} />;
-
-  return <Image src="/logo-black.png" alt="logo" width={83} height={50} />;
+  return <Image src="/logo-light.png" alt="logo" width={250} height={150} />;
 }
