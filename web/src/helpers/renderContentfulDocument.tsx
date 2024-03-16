55559544e135b157acfe7af7caebdb00cd720436
Hoai Thai <khanhhoai95@gmail.com>
2024-03-16 09:13:11 +0700

    feat: handle assets for renderContentfulDocument

--- web/src/helpers/renderContentfulDocument.tsx
+++ web/src/helpers/renderContentfulDocument.tsx
@@ -1,7 +1,24 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { Document } from "@contentful/rich-text-types";
+import { BLOCKS, Document } from "@contentful/rich-text-types";
+import { Center, Stack, Text } from "@mantine/core";
 
 export const renderContentfulDocument = (doc: Document) => {
   // TODO: Extend to pass option
-  return documentToReactComponents(doc)
-}
+  return documentToReactComponents(doc, {
+    preserveWhitespace: true,
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+        return (
+          <Stack align="center" gap="xs">
+            <img
+              src={`https:${node.data.target.fields.file.url}`}
+              width="100%"
+              alt={node.data.target.fields.description}
+            />
+            <Text size="sm">{node.data.target.fields.title}</Text>
+          </Stack>
+        );
+      },
+    },
+  });
+};
