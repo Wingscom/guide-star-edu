b01101134f94da4623e310db10e198db929ab398
Hoai Thai <khanhhoai95@gmail.com>
2024-03-25 18:28:20 +0700

    feat: create course table

--- backend/schools/models.py
+++ backend/schools/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 class School(models.Model):
   name = models.CharField(max_length=30,blank=False)
-  country = models.CharField(max_length=30,blank=False)
+  country = models.CharField(max_length=3,blank=False)
   state = models.CharField(max_length=20,blank=False)
   city = models.CharField(max_length=20,blank=False)
   sources = models.JSONField(blank=False)
+
+class Course(models.Model):
+  name = models.CharField(max_length=100,blank=False)
+  school = models.ForeignKey(
+    School,
+    on_delete=models.CASCADE,
+  )
+  commission = models.CharField(max_length=20)
+  tuition_fee = models.CharField(max_length=20)
+  application_fee = models.CharField(max_length=20)
+  duration = models.CharField(max_length=20)
+  intake = models.CharField(max_length=20)
+  sector = models.CharField(max_length=20,blank=False)
