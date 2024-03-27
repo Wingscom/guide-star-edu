76cce8904f3886a9ce9fdfb441d97f9bbd9f6f7b
Hoai Thai <khanhhoai95@gmail.com>
2024-03-27 16:34:20 +0700

    conf: update allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'api.guidestaredu.com',
+    'localhost',
+]
 
 
 # Application definition
