2751283ba19559d0d18ba98b9abd31ed7ac5a32b
vinhpm <phanminhvinh3010023@gmail.com>
2025-09-29 12:07:42 +0700

    feat: format and import list travels at header

--- web/src/app/[locale]/_components/ui/AppShell/Header/Header.tsx
+++ web/src/app/[locale]/_components/ui/AppShell/Header/Header.tsx
@@ -11,92 +11,89 @@ import { ThemeToggler } from "../ThemeToggler/ThemeToggler";
 import classes from "./Header.module.css";
 import { HeaderItemsDesktop } from "./HeaderItemsDesktop";
 import { HeaderItemsMobile } from "./HeaderItemsMobile";
-import { getOverviewsMenu } from "./helpers/getOverviewMenu";
+import { getListTravelCountry, getOverviewsMenu } from "./helpers/getOverviewMenu";
 
 export type HeaderItem = {
-  link: string;
-  label: string;
-  menu?: {
     link: string;
     label: string;
     menu?: {
-      link: string;
-      label: string;
+        link: string;
+        label: string;
+        menu?: {
+            link: string;
+            label: string;
+        }[];
     }[];
-  }[];
 };
 
 export default function Header() {
-  const locale = useCurrentLocale();
-  const headerT = useScopedI18n("header");
-  const links = getAppLinks(locale);
-  const overviewMenu = useAsync(() => getOverviewsMenu(locale));
+    const locale = useCurrentLocale();
+    const headerT = useScopedI18n("header");
+    const links = getAppLinks(locale);
+    const overviewMenu = useAsync(() => getOverviewsMenu(locale));
+    const listCountryTravels = useAsync(() => getListTravelCountry(locale));
 
-  const [isBurgerOpen, { toggle: toggleBurger, close: closeBurger }] =
-    useDisclosure(false);
+    const [isBurgerOpen, { toggle: toggleBurger, close: closeBurger }] = useDisclosure(false);
 
-  const items: HeaderItem[] = [
-    {
-      link: links.home(),
-      label: headerT("labels.home"),
-    },
-    {
-      link: links.courses(),
-      label: headerT("labels.courses"),
-    },
-    {
-      link: "#",
-      label: headerT("labels.studyabroad"),
-      menu: overviewMenu.result ?? [],
-    },
-    {
-      link: links.immigrations(),
-      label: headerT("labels.immigrations"),
-    },
-    {
-      link: links.blogs(),
-      label: headerT("labels.blogs"),
-      menu: [
+    const items: HeaderItem[] = [
         {
-          link: links.news(),
-          label: headerT("labels.news"),
+            link: links.home(),
+            label: headerT("labels.home"),
         },
         {
-          link: links.events(),
-          label: headerT("labels.events"),
+            link: links.travels(),
+            label: headerT("labels.travel"),
+            menu: listCountryTravels.result ?? [],
         },
         {
-          link: links.scholarships(),
-          label: headerT("labels.scholarships"),
+            link: "#",
+            label: headerT("labels.studyabroad"),
+            menu: overviewMenu.result ?? [],
         },
-      ],
-    },
-    {
-      link: links.info(),
-      label: headerT("labels.info"),
-    },
-  ];
+        {
+            link: links.immigrations(),
+            label: headerT("labels.immigrations"),
+        },
+        {
+            link: links.blogs(),
+            label: headerT("labels.blogs"),
+            menu: [
+                {
+                    link: links.news(),
+                    label: headerT("labels.news"),
+                },
+                {
+                    link: links.events(),
+                    label: headerT("labels.events"),
+                },
+                {
+                    link: links.scholarships(),
+                    label: headerT("labels.scholarships"),
+                },
+            ],
+        },
+        {
+            link: links.info(),
+            label: headerT("labels.info"),
+        },
+    ];
 
-  return (
-    <AppShell.Header>
-      <Container size="lg">
-        <div className={classes.inner}>
-          <AppLogo />
-          <Group gap={5} wrap="nowrap">
-            <HeaderItemsDesktop items={items} />
-            <ThemeToggler />
-            <LangToggler />
-            <Burger
-              opened={isBurgerOpen}
-              onClick={toggleBurger}
-              hiddenFrom="sm"
-            />
-          </Group>
-        </div>
-      </Container>
-      <Collapse in={isBurgerOpen} hiddenFrom="sm">
-        <HeaderItemsMobile items={items} onClose={closeBurger} />
-      </Collapse>
-    </AppShell.Header>
-  );
+    return (
+        <AppShell.Header>
+            <Container size="lg">
+                <div className={classes.inner}>
+                    <AppLogo />
+                    <Group gap={5} wrap="nowrap">
+                        <HeaderItemsDesktop items={items} />
+                        <ThemeToggler />
+                        <LangToggler />
+                        <Burger opened={isBurgerOpen} onClick={toggleBurger} hiddenFrom="sm" />
+                    </Group>
+                </div>
+            </Container>
+            <Collapse in={isBurgerOpen} hiddenFrom="sm">
+                <HeaderItemsMobile items={items} onClose={closeBurger} />
+            </Collapse>
+        </AppShell.Header>
+    );
 }
