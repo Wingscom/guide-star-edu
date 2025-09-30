b8b2f51d18705304d23dbaf4c5bc09ae79a264f3
CarlavierVN <namto10042004@gmail.com>
2025-09-30 11:56:54 +0700

    [add] travels popup

--- web/src/app/[locale]/travels/[slug]/page.tsx
+++ web/src/app/[locale]/travels/[slug]/page.tsx
@@ -4,6 +4,8 @@ import { getTravelDetailContent } from "./action";
 import { getProcessingApplicationVisa } from "../action";
 import { ProcessApplication } from "../_components";
 import { richTextFromMarkdown } from "@contentful/rich-text-from-markdown";
+import WrapperSignUpForm from "../_components/WrapperSignupForm";
+import { getScopedI18n } from "@/locales/server";
 
 export default async function TravelDetailPage({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
@@ -14,13 +16,15 @@ export default async function TravelDetailPage({ params }: { params: Promise<{ s
     const document = await richTextFromMarkdown(travelContent.description as unknown as string);
 
     return (
-        <Stack>
+        <Stack style={{ position: "relative" }}>
             <Title>{travelContent.title}</Title>
             {renderContentfulDocument(document)}
             <ProcessApplication
                 steps={processingVisaContent.steps}
                 title={processingVisaContent.title}
             />
+            <div style={{ height: 30 }} />
+            <WrapperSignUpForm />
         </Stack>
     );
 }
